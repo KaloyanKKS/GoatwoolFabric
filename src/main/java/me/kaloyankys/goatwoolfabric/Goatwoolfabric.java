36f373b87aa14349531a13e9b3b404f17bfb7715
KaloyanKKS <k.yunchov9@gmail.com>
2021-06-09 18:06:52 +0100

    Initial commit

---
+++ src/main/java/me/kaloyankys/goatwoolfabric/Goatwoolfabric.java
@@ -0,0 +1,9 @@
+package me.kaloyankys.goatwoolfabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Goatwoolfabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/me/kaloyankys/goatwoolfabric/client/GoatwoolfabricClient.java
@@ -0,0 +1,11 @@
+package me.kaloyankys.goatwoolfabric.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+public class GoatwoolfabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
