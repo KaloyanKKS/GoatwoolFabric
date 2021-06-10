34bf9c0a643f3f24090a4c564ec1ff5286eb8b85
PeterGamesGR <46004440+PeterGamesGR@users.noreply.github.com>
2021-06-10 15:44:28 +0300

    I did some things lol.

---
+++ src/main/java/io/github/llamarama/team/goatwoolfabric/GoatWoolFabric.java
@@ -0,0 +1,12 @@
+package io.github.llamarama.team.goatwoolfabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class GoatWoolFabric implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+
+}
